9af7f995398d2df439e9b925b873365af56985ff
Vinícius Silva Pereira <viniciussilvapereira24@gmail.com>
2022-08-11 21:04:37 -0300

    changes

--- src/main/java/com/example/library/connectionDatabase/IAuthorRepository.java
+++ src/main/java/com/example/library/connectionDatabase/IAuthorRepository.java
@@ -2,9 +2,12 @@ package com.example.library.connectionDatabase;
 
 import com.example.library.model.Author;
 
+import java.util.List;
+
 public interface IAuthorRepository {
     public void save(Author author);
     public void update(Author author);
-    public void delete(Author author);
-    public Author findById(String id);
+    public void delete(String id);
+    public String findById(String id);
+    public List<String> findAll();
 }
